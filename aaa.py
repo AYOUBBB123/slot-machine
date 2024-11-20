76a6989d17166cd02a84c96bab4040a62f3024e5
Ayoubbezai2024 <158614538+AYOUBBB123@users.noreply.github.com>
2024-11-20 02:21:39 +0100

    Add files via upload

---
+++ aaa.py
@@ -0,0 +1,23 @@
+# r for reading a file
+
+# when u add the b in any character it became for binary text
+# for exemple an image
+
+# r+ Opens a file for both reading and writing
+
+# w for write only
+
+# b for binary file and t for text
+
+# + for updating (read and write )
+
+# w+ for reading or writing ,Overwrites the existing file
+# if the file exists. If the file does not exist, creates a new file for
+
+# a for append if does not exists creat one for writing
+
+# a+ same but the file for r and w
+
+# x File does not exist: The file is created, and content can be written to it.
+# File exists: A FileExistsError is raised, preventing any modification to the existing file.
+
